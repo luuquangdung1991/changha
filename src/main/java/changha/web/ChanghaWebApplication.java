87b98fd72fa883a0f92b1ef6c8219fa00826d41e
QuangDungLuu <quang-dung.luu@deutschebahn.com>
2024-06-05 23:25:28 +0200

    add dependency for web and add function helloworld

--- src/main/java/changha/web/ChanghaWebApplication.java
+++ src/main/java/changha/web/ChanghaWebApplication.java
@@ -2,6 +2,8 @@ package changha.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ChanghaWebApplication {
@@ -10,4 +12,12 @@ public class ChanghaWebApplication {
 		SpringApplication.run(ChanghaWebApplication.class, args);
 	}
 
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+}
 }
